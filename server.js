2743937317f33c6732b1bd6dbe7817571a129875
Antonio Carlos <antonio.carlos@verifiable.com>
2022-05-09 22:59:16 -0300

    create file on request

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const http = require('http');
-const port = 4000;
+const fs = require('fs');
 
-const ip = 'localhost'
+const port = 4000;
 
 http.createServer((req, res) => { 
     console.log('Recebendo uma request!');
     res.end('Aqui fica o que vamos enviar para o navegador como resposta!');
     req.on('data', chunk => {
         console.log(`Data chunk available: ${chunk}`);
-      });
+
+        fs.appendFile('file.log', chunk, err => {
+          if (err) {
+            console.error(err);
+          }
+        });
+    });
 
 }).listen(port);
