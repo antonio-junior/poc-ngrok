7f0f7f2330be81d49efe13142ee57b8c1ba5b278
Antonio Carlos <antonio.carlos@verifiable.com>
2022-05-09 23:24:51 -0300

    fix server method

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const fs = require('fs');
 
 const port = 4000;
 
-http.createServer((req, res) => {
-    req.on('data', chunk => {
-        console.log(`Data chunk available: ${chunk}`);
+http.createServer((req, res) => { 
+  const filePath = 'file.log';
 
-        fs.appendFile('file.log', chunk, err => {
-          if (err) {
-            console.error(err);
-          }
-        });
-    });
+  fs.unlinkSync(filePath);
+
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+
+  req.on('data', chunk => {
+      console.log(`Data chunk available: ${chunk}`);
+
+      fs.appendFile(filePath, chunk, err => {
+        if (err) {
+          console.error(err);
+        }
+      });
+      res.end();
+  });
+    
 }).listen(port);
